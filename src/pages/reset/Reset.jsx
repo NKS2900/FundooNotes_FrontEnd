39bed267a4f570df80d3f2b3b2badef0863250d5
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-09 17:40:42 +0530

    [NIJAM] Add: Reset password component added.

---
+++ src/pages/reset/Reset.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { TextField, Button,Card } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import '../reset/reset.css';
+
+class Reset extends Component{
+  constructor(props){
+    super(props)
+  }
+
+//     handleChange=()=>{
+//       this.props.history.push("/signup");
+//   };
+    render(){
+        return(
+            <div className="maindiv">
+                <Card className="resetcard">
+                <div id="fundoohead">
+                <ArrowBackIcon id='backarrows' onClick={()=>this.props.history.push("/login")}/>
+                <div className="fundooHeader">
+                      <div className="f">F</div>
+                      <div className="u">u</div>
+                      <div className="n">n</div>
+                      <div className="d">d</div>
+                      <div className="o">o</div>
+                      <div className="oo">o</div>
+                    </div>
+                    </div>
+                    <div id="text-fieldss">
+                    <TextField id="textbox12" label="Email" required variant="outlined" /> <br/><br/>
+                    <TextField id="textbox12" label="New Password" required variant="outlined" /> <br/><br/>
+                    <TextField id="textbox12" label="Confirm New Password" required variant="outlined" />   <br/><br/>                                                              
+                    <div><Button id="forgotbtn" variant="contained">Submit</Button></div>
+                    </div>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default Reset;
+

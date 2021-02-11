b1a0364ac66aad89d0e85d2afd047b3b2993372e
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-11 13:31:06 +0530

    [NIJAM] Add: API integration done for Reset page.

--- src/pages/reset/Reset.jsx
+++ src/pages/reset/Reset.jsx
@@ -2,15 +2,74 @@ import React, { Component } from 'react';
 import { TextField, Button,Card } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import '../reset/reset.css';
+import { reset } from '../../services/UserService.js';
 
 class Reset extends Component{
   constructor(props){
-    super(props)
+    super(props);
+    this.state = { 
+      email: '', 
+      password: '', 
+      snackbarOpen: false,
+      snackbarMessage: "",
+    };
   }
 
-//     handleChange=()=>{
-//       this.props.history.push("/signup");
-//   };
+  SnackbarClose = (e) => {
+    this.setState({ snackbarOpen: false });
+  };
+
+  handleCloseSnackbar = () => {
+    this.setState({ snackbarOpen: false });
+  };
+
+  handleSubmit=()=>{
+    let loginData ={
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    reset(loginData).then((response)=>{
+      if(response.status === 200){
+        alert("Password Changed Successfully...");
+        
+        this.setState({
+          snackbarOpen: true,
+          snackbarMessage: "Login Succesfully.",
+        }
+        );
+        this.props.history.push("/login");
+      }
+      else {
+        this.setState({
+          snackbarOpen: true,
+          snackbarMessage: "Enter correct credentials",
+        });
+      }
+      console.log("Login Response : ",response);
+    }
+    
+    ).catch((err)=>{console.log(err);})
+  }
+
+  handleEmail=(event)=>{
+    this.setState({ email:event.target.value});
+    this.state.email = event.target.value;
+    console.log("Email: ", this.state.email);
+  }
+
+  handlePassword=(event)=>{
+    this.setState({ password:event.target.value});
+    this.state.password = event.target.value;
+    console.log("Password:", this.state.password);
+  }
+
+  handleCpass=(event)=>{
+    this.setState({ cpassword:event.target.value});
+    this.state.cpassword = event.target.value;
+    console.log("CPassword:", this.state.cpassword);
+  }
+
     render(){
         return(
             <div className="maindiv">
@@ -27,10 +86,10 @@ class Reset extends Component{
                     </div>
                     </div>
                     <div id="text-fieldss">
-                    <TextField id="textbox12" label="Email" required variant="outlined" /> <br/><br/>
-                    <TextField id="textbox12" label="New Password" required variant="outlined" /> <br/><br/>
-                    <TextField id="textbox12" label="Confirm New Password" required variant="outlined" />   <br/><br/>                                                              
-                    <div><Button id="forgotbtn" variant="contained">Submit</Button></div>
+                    <TextField id="textbox12" label="Email" onChange={this.handleEmail} required variant="outlined" /> <br/><br/>
+                    <TextField id="textbox12" label="New Password" onChange={this.handlePassword} required variant="outlined" /> <br/><br/>
+                    <TextField id="textbox12" label="Confirm New Password" onChange={this.handleCpass} required variant="outlined" />   <br/><br/>                                                              
+                    <div><Button id="forgotbtn" variant="contained" onClick={this.handleSubmit}>Submit</Button></div>
                     </div>
                 </Card>
             </div>

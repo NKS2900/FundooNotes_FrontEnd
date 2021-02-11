42358f8b6457c6300433ede69a3ced6d95eb8533
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-11 13:29:28 +0530

    [NIJAM] Add: API integration done for Signup page.

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
 import Card from '@material-ui/core/Card';
 import { TextField, Button, InputAdornment } from '@material-ui/core';
 import '../signup/signup.css';
 import VisibilityIcon from '@material-ui/icons/Visibility';
+import { signup } from '../../services/UserService.js';
 
 class Signup extends Component{
+  constructor(props){
+    super(props);
+    this.state = { fname:'',lname:'',email: '', password: '', cpassword:'' };
+  }
+
+  handleSubmit=()=>{
+    let signupData ={
+      firstName: this.state.fname,
+      lastName: this.state.lname,
+      email: this.state.email,
+      password: this.state.password
+      
+    }
+    // let email= this.state.email;
+    // let password = this.state.password;
+    // let response = login(loginData);
+    // console.log("Login Response: ", response);
+    signup(signupData).then((response)=>{
+      if(response.status === 200){
+        alert("Registration successfull...");
+        this.props.history.push("/login");
+      }
+      console.log("Signup Response : ",response);
+    }).catch((err)=>{console.log(err);})
+  }
+
+  handleFname=(event)=>{
+    this.setState({ fname:event.target.value});
+    this.state.fname = event.target.value;
+    console.log("Fname: ", this.state.fname);
+  }
+
+  handleLname=(event)=>{
+    this.setState({ lname:event.target.value});
+    this.state.lname = event.target.value;
+    console.log("Lname: ", this.state.lname);
+  }
+
+  handleEmail=(event)=>{
+    this.setState({ email:event.target.value});
+    this.state.email = event.target.value;
+    console.log("Email: ", this.state.email);
+  }
+
+  handlePassword=(event)=>{
+    this.setState({ password:event.target.value});
+    this.state.password = event.target.value;
+    console.log("Password:", this.state.password);
+  }
+    
+  handleCpass=(event)=>{
+    this.setState({ cpassword:event.target.value});
+    this.state.cpassword = event.target.value;
+    console.log("CPassword:", this.state.cpassword);
+  }
+  
     render(){
         return(
             <div className="containers">
@@ -18,14 +76,14 @@ class Signup extends Component{
                       <div className="oo">o</div>
                     </div>
                     <div id="name">
-                    <div id="fname"><TextField  label="First Name" required variant="outlined" /></div>
-                    <div id="lname"><TextField  label="Last Name" required variant="outlined" /></div>
+                    <div id="fname"><TextField  label="First Name" onChange={this.handleFname} required variant="outlined" /></div>
+                    <div id="lname"><TextField  label="Last Name" onChange={this.handleLname} required variant="outlined" /></div>
                         </div>
                     <div id="emaild">
-                    <TextField id="email" label="Email" required variant="outlined" />  
+                    <TextField id="email" label="Email" onChange={this.handleEmail} required variant="outlined" />  
                     </div>
                     <div id="password1">
-                    <TextField id="password" label="Password" required variant="outlined" 
+                    <TextField id="password" label="Password" onChange={this.handlePassword} required variant="outlined" 
                      InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
@@ -33,7 +91,7 @@ class Signup extends Component{
                           </InputAdornment>),}}/>
                     </div>
                     <div id="password2">
-                    <TextField id="cpassword" label="Confirm Password" required variant="outlined" 
+                    <TextField id="cpassword" label="Confirm Password" onChange={this.handleCpass} required variant="outlined" 
                      InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
@@ -44,7 +102,7 @@ class Signup extends Component{
                     />
                     </div>
                     <div id="SignUpBtn">
-                    <Button id="signupbt" variant="contained">SignUp</Button>
+                    <Button id="signupbt" variant="contained" onClick={this.handleSubmit} >SignUp</Button>
                     <label id="signmsg">Have an account with us  <a href='/login'>Login</a></label>
                     </div>
                 </Card>
@@ -53,5 +111,5 @@ class Signup extends Component{
     }
 }
 
-export default Signup;
+export default withRouter(Signup);
 

f2abcd66dcccfc382014b6881d557513e888006b
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-11 13:27:32 +0530

    [NIJAM] Add: API integration done for Login page.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,77 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
 import Card from '@material-ui/core/Card';
-import { TextField, Button, InputAdornment } from '@material-ui/core';
+import { TextField, Button, InputAdornment, helperText, Snackbar ,error} from '@material-ui/core';
 import '../login/login.css';
 import VisibilityIcon from '@material-ui/icons/Visibility';
+import { login } from '../../services/UserService.js';
 
 class Login extends Component{
-  constructor(props){
-    super(props)
-  }
+    constructor(props){
+      super(props);
+      this.state = { 
+        email: '', 
+        password: '', 
+        emailerror:'',
+        mobilerror:'',
+        eerror: false,
+        merror: false,
+        snackbarOpen: false,
+        snackbarMessage: "",
+      };
+    }
 
-    handleChange=()=>{
+    SnackbarClose = () => {
+      this.setState({ snackbarOpen: false });
+    };
+  
+    handleCloseSnackbar = () => {
+      this.setState({ snackbarOpen: false });
+    };
+
+
+    handleLogin=()=>{
+      
+      let loginData ={
+        email: this.state.email,
+        password: this.state.password
+      }
+
+      login(loginData).then((response)=>{
+        if(response.status === 200){
+          alert("Login Successfull...");
+          this.setState({
+            snackbarOpen: true,
+            snackbarMessage: "Login",
+          }
+          );
+        }
+        else {
+          this.setState({
+            snackbarOpen: true,
+            snackbarMessage: "Enter correct credentials",
+          });
+        }
+        console.log("Login Response : ",response);
+      }
+      ).catch((err)=>{console.log(err);})
+  }
+  
+    handleEmail=(event)=>{
+      this.setState({ email:event.target.value});
+      this.state.email = event.target.value;
+      console.log("Email: ", this.state.email);
+    }
+  
+    handlePassword=(event)=>{
+      this.setState({ password:event.target.value});
+      this.state.password = event.target.value;
+      console.log("Password:", this.state.password);
+    }
+    handleSignup=()=>{
       this.props.history.push("/signup");
   };
+
     render(){
         return(
             <div className="container">
@@ -25,8 +85,12 @@ class Login extends Component{
                       <div className="oo">o</div>
                     </div>
                     <div id="text-field">
-                    <TextField id="textbox1" label="Email" required variant="outlined" />                                                                    <br/><br/>
-                    <TextField id="textbox2" label="Password" required variant="outlined" 
+                    <TextField id="textbox1" label="Email"  variant="outlined"
+                     onChange={this.handleEmail}  helperText={this.state.emailerror}  
+                     error={this.state.eerror}/>                                                                    <br/><br/>
+                    
+                    <TextField id="textbox2"  label="Password" variant="outlined" 
+                     onChange={this.handlePassword} helperText={this.state.mobilerror} error={this.state.merror}  
                      InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
@@ -38,19 +102,28 @@ class Login extends Component{
                     <label id="forgotmsg"><a href='/forgot'>Forgot Password</a></label>
                     </div>
                     <div id="LoginBtn">
-                    <Button id="loginbtn" variant="contained">Login</Button>
+                    <Button type="submit" id="loginbtn" variant="contained" onClick={this.handleLogin}>Login</Button>
                     </div>
                     <div id="SignUpBtn">
                     <div id="signup">
                     <label id="msg">Don't have an account with us ?</label>
                     </div>
-                    <Button id="loginbtn" variant="contained" onClick= {this.handleChange}>SignUp</Button>
+                    <Button id="loginbtn" variant="contained" onClick={this.handleSignup}>SignUp</Button>
                     </div>
                 </Card>
+                      <Snackbar
+                          anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                          }}
+                          open={this.state.snackbarOpen}
+                          autoHideDuration={5000}
+                          massege={this.state.snackbarMessage}
+                        ></Snackbar>
             </div>
         )
     }
 }
 
-export default Login;
+export default withRouter(Login);
 

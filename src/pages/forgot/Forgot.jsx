5eaa422e28ddbfd43336a602a2f5ed4b224163f4
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-11 13:33:10 +0530

    [NIJAM] Add: API integration done for Forgot password page.

--- src/pages/forgot/Forgot.jsx
+++ src/pages/forgot/Forgot.jsx
@@ -2,16 +2,59 @@ import React, { Component } from 'react';
 import { TextField, Button,Card } from '@material-ui/core';
 import '../forgot/forgot.css';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-
+import { forgot } from '../../services/UserService.js';
 
 class Forgot extends Component{
-  constructor(props){
-    super(props)
-  }
+    constructor(props){
+        super(props);
+        this.state = { 
+          email: '', 
+          snackbarOpen: false,
+          snackbarMessage: "",
+        };
+      }
+  
+      SnackbarClose = (e) => {
+        this.setState({ snackbarOpen: false });
+      };
+    
+      handleCloseSnackbar = () => {
+        this.setState({ snackbarOpen: false });
+      };
+  
+      handleSend=()=>{
+         let Email= this.state.email;
+        // let loginData ={
+        //   email: this.state.email, 
+        // }
+        forgot(Email).then((response)=>{
+          if(response.status === 200){
+            alert("Send Successfully...");
+            this.props.history.push("/login");
+            this.setState({
+              snackbarOpen: true,
+              snackbarMessage: "Login Succesfully.",
+            }
+            );
+          }
+          else {
+            this.setState({
+              snackbarOpen: true,
+              snackbarMessage: "Enter correct credentials",
+            });
+          }
+          console.log("Login Response : ",response);
+        }
+        
+        ).catch((err)=>{console.log(err);})
+      }
+    
+      handleEmail=(event)=>{
+        this.setState({ email:event.target.value});
+        this.state.email = event.target.value;
+        console.log("Email: ", this.state.email);
+      }
 
-//     handleChange=()=>{
-//       this.props.history.push("/signup");
-//   };
     render(){
         return(
             <div className="maindiv">
@@ -28,8 +71,8 @@ class Forgot extends Component{
                     </div>
                     </div>
                     <div id="text-fields">
-                    <TextField id="textbox11" label="Email" required variant="outlined" />                                                                    <br/><br/>
-                    <Button id="forgotbtn" variant="contained">Submit</Button>
+                    <TextField id="textbox11" label="Email" onChange={this.handleEmail} required variant="outlined" />                                                                    <br/><br/>
+                    <Button id="forgotbtn" variant="contained" onClick={this.handleSend}>Submit</Button>
                     </div>
                 </Card>
             </div>

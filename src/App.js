9ff397dbb59ce08ee1b416a7174122d50f1cf228
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-13 09:31:36 +0530

    [NIJAM] Add: Home Component added as Parent Component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import {BrowserRouter as Router,  Route, Switch} from 'react-router-dom'
 
 import Login from './pages/login/Login.jsx';
 import Signup from './pages/signup/Signup.jsx'
+import Forgot from './pages/forgot/Forgot.jsx'
+import Reset from './pages/reset/Reset.jsx'
+import Home from '../src/pages/Home/Home.jsx'
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +15,9 @@ function App() {
       <Switch>
           <Route path="/login" component={ Login } />
           <Route path="/signup" component={ Signup } />
+          <Route path="/forgot" component={ Forgot } />
+          <Route path="/reset" component={ Reset } />
+          <Route path="/home" component={ Home } />
       </Switch>
       </Router>
     </div>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import AppBar from '../../components/AppBar/Appbar.jsx'
+import '../Home/home.css'
+
+export default function Home() {
+  return (
+    <div>
+      <AppBar />
+      <div className="card-body text-center" >
+        <div className="main">
+        </div>
+      </div>
+    </div>
+  )
+}

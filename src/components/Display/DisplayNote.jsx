ffdfb39ae806eeae3ed24a3632c9bd6746527623
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-22 19:17:05 +0530

    [NIJAM] Refactor: Changes added in DisplayNote Component.

--- src/components/Display/DisplayNote.jsx
+++ src/components/Display/DisplayNote.jsx
@@ -1,66 +1,86 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import service from '../../services/NoteService.js';
 import './displayNote.scss'
 import DisplayIcons from '../DisplayIcon/DisplayIcon.jsx'
-import Pin from '../../assets/pin.jpeg'
-import Unpin from '../../assets/unpin.jpeg'
-import { Avatar,Chip } from '@material-ui/core';
+import Pin from '../../assets/pin.png'
+import Unpin from '../../assets/unpin.png'
+import { Avatar, Chip } from '@material-ui/core';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 
-const DisplayNote= (props) => {
-    const [noteList, setNoteList] = useState([]);
-    const [pin, setPin] = useState(false);
-    // const response=service.getAllNote();
-    //  setNoteList(response.data);
+const DisplayNote = (props) => {
+  const [noteList, setNoteList] = useState([]);
+  const [pin, setPin] = useState(false);
+  const [chipId,setChipId]=useState('');
+  const [bgColor, setBgColor] = useState('#fff')
+  // const response=service.getAllNote();
+  //  setNoteList(response.data);
 
-    const GetNote = () => {
-      let userId=localStorage.getItem('userId');
-        service.getAllNote(userId)
-        .then((response) => {
-          if(response.status === 200){
-            setNoteList(response.data.data);
-          }
-        })
-        .catch(() => {
-          console.log("Error While Fetching Notes!!!")
-        });
-    }
+  // const GetNote = () => {
+  //   let userId=localStorage.getItem('userId');
+  //     service.getAllNote(userId)
+  //     .then((response) => {
+  //       if(response.status === 200){
+  //         setNoteList(response.data.data);
+  //       }
+  //     })
+  //     .catch(() => {
+  //       console.log("Error While Fetching Notes!!!")
+  //     });
+  // }
 
-        useEffect(() => {
-            GetNote()
-        }, []);
-        
-    const handlePin = () => {
-      setPin(true);
-    }
-    const handleUnPin = () => {
-      setPin(false);
-    }
-    return(
- 
-          <div id="display-note-container">
-          <div className="card" >
-                  {noteList.reverse().map((item) => (
-                    <div className="addNote" style={{ backgroundColor: item.colour }} key={item.noteId}>
-                       <div className="notes1"  >
-                         {/* <div className="noteImage"><img src={item.image}/></div> */}
-                          <div className="note pds"><b>{item.title}</b>
-                          {/* <div>{pin ? <img id="pin" src={Pin} onClick={handleUnPin}/> : <img id="pin" src={Unpin} onClick={handlePin}/> }</div> */}
-                          </div>
-                          <div className='note pds'>{item.description}</div>
-                          <div className="Chip">
-                            {item.reminder}
-                          {/* <Chip avatar={<Avatar><AccessAlarmIcon/></Avatar>} label={item.reminder}  /> */}
-                          </div>
-                      <div className="cardActions"><DisplayIcons item={item} GetNote={GetNote}/></div>
-                    </div> 
-                     </div>
-		      	    ))}
+  //     useEffect(() => {
+  //         GetNote()
+  //     }, []);
+
+  const handlePin = () => {
+    setPin(true);
+  }
+  const handleUnPin = () => {
+    setPin(false);
+  }
+  
+  const handleReminderChip = (noteId) => {
+    console.log("Note_ID: ",noteId);
+        service.UnSetReminder(noteId).then(res => {
+            console.log(res)
+        props.GetNote()
             
-            </div>
+        }).catch(err => {
+            console.log(err);
+        })
+  }
+
+    //console.log(props.GetNote())
+
+
+  return (
+
+    <div id="display-note-container">
+      <div className="card" >
         
-        </div>
-     
-    );
+        {props.item.reverse().map((item) => (
+          <div className="addNote" style={{ backgroundColor: item.colour }} key={item.noteId}>
+            <div className="notes1"  >
+              {/* <div className="noteImage"><img src={item.image}/></div> */}
+              <div className="note pds"><b>{item.title}</b>
+                {/* <div>
+                  {item.pin?
+                      <div>{pin ? <img id="pin" src={Pin} onClick={handleUnPin}/> : <img id="pin" src={Unpin} onClick={handlePin}/> }</div> 
+                      :false}
+                </div> */}
+              </div>
+              <div className='note pds'>{item.description}</div>
+         
+                {/* {item.reminder} */}
+                {item.reminder?
+                <Chip className="noteChip" avatar={<Avatar><AccessAlarmIcon/></Avatar>} label={item.reminder} onDelete={e => handleReminderChip(item.noteId)} />
+                  :null}
+              <div className="cardActions"><DisplayIcons setBgColor={setBgColor} item={item} GetNote={props.GetNote} /></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default DisplayNote

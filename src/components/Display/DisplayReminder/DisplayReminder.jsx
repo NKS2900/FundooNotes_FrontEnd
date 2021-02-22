d1ba57366ef7b4d63801e13d65efb655fdb1a630
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-22 21:46:51 +0530

    [NIJAM] Refactor: Changes added in Display Reminder using DisplayComponent for DisplayReminder.

--- src/components/Display/DisplayReminder/DisplayReminder.jsx
+++ src/components/Display/DisplayReminder/DisplayReminder.jsx
@@ -7,25 +7,24 @@ import Note from '../../Note/Note.jsx'
 import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
-const DisplayReminder= () => {
+import DisplayNote from '../DisplayNote.jsx'
+
+const DisplayReminder= ({ GetNote }) => {
     const [noteList, setNoteList] = useState([]);
     
-    // const response=service.getAllNote();
-    //  setNoteList(response.data);
-
     const getReminders = () => {
       let userId=localStorage.getItem('userId');
         service.getReminder(userId)
         .then((response) => {
           if(response.status === 200){
             setNoteList(response.data.data);
+            GetNote()
           }
         })
         .catch(() => {
           console.log("Error While Fetching Notes!!!")
         });
     }
-
         useEffect(() => {
             getReminders()
         }, []);
@@ -33,23 +32,8 @@ const DisplayReminder= () => {
     return(
           <div>
             <AppBar />
-            <div className="createNote"><Note /></div>
-          <div id="display-note-reminder">
-          <div className="cardss" >
-                  {noteList.map((items) => (
-                    <div className="addNote" style={{ backgroundColor: items.colour }} key={items.noteId}>
-                       <div className="notes1" >
-                          <div className="note pds"><b>{items.title}</b></div>
-                          <div className='note pds'>{items.description}</div>
-                          <div className="Chip">
-                            <Chip avatar={<Avatar><AccessAlarmIcon/></Avatar>} label={items.reminder}  /></div>
-                          {/* <div className='note pds'>{items.reminder}</div> */}
-                          <div className="cardActionIcon"><DisplayIcons item={items} GetNote={getReminders}/></div>
-                      </div> 
-                     </div>
-		      	    ))}
-            </div>
-            </div>
+            <div className="createNote"><Note item={noteList} GetNote={getReminders}/></div>
+            <div className="displayNotes"><DisplayNote item={noteList} GetNote={getReminders}/></div>
         </div>
     );
 }

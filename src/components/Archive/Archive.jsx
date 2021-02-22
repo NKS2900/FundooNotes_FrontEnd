7e5d5e914723c33bcc383b02b74f43ee1893fbc6
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-22 21:48:05 +0530

    [NIJAM] Refactor: Changes added in Display Archive use of DisplayComponent for DisplayArchive.

--- src/components/Archive/Archive.jsx
+++ src/components/Archive/Archive.jsx
@@ -1,51 +1,35 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import service from '../../services/NoteService.js';
 import DisplayIcons from '../DisplayIcon/DisplayIcon.jsx'
 import '../Archive/archive.scss'
 import AppBar from '../AppBar/Appbar.jsx'
+import DisplayNote from '../Display/DisplayNote.jsx'
 
 const ArchiveNote= () => {
     const [noteList, setNoteList] = useState([]);
-    
-    // const response=service.getAllNote();
-    //  setNoteList(response.data);
 
-    const getArchiveNotes = () => {
-      let userId=localStorage.getItem('userId');
-        service.getArchiveNote(userId)
-        .then((response) => {
-          if(response.status === 200){
-            setNoteList(response.data.data);
-          }
-        })
-        .catch(() => {
-          console.log("Error While Fetching Notes!!!")
-        });
-    }
+  const getArchiveNotes = () => {
+    let userId = localStorage.getItem('userId');
+    service.getArchiveNote(userId)
+      .then((response) => {
+        if (response.status === 200) {
+          setNoteList(response.data.data);
+        }
+      })
+      .catch(() => {
+        console.log("Error While Fetching Notes!!!")
+      });
+  }
 
-        useEffect(() => {
-          getArchiveNotes()
-        }, []);
+  useEffect(() => {
+    getArchiveNotes()
+  }, []);
         
-    return(
-          <div>
-            <AppBar />
-          <div id="display-note-container">
-          <div className="cardss" >
-                  {noteList.map((item) => (
-                    <div className="addNote" style={{ backgroundColor: item.colour }}key={item.noteId}>
-                       <div className="notes1" >
-                          <div className="note pds"><b>{item.title}</b></div>
-                          <div className='note pds'>{item.description}</div>
-                          <div className="cardActionIcon">
-                            <DisplayIcons item={item} GetNote={getArchiveNotes}/></div>
-                      </div> 
-                     </div>
-		      	    ))}
-            
-            </div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <AppBar />
+      <div className="displayArchive"><DisplayNote item={noteList} GetNote={getArchiveNotes}/></div>
+    </div>
+  );
 }
 export default ArchiveNote
